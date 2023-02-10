d449b6e9a7944ef32f9abcf19fe584b515a7294c
Daniel Williams <dwilli@kth.se>
2023-02-10 13:47:42 +0100

    Complete S1C2

--- src/set1.rs
+++ src/set1.rs
@@ -1,1 +1,9 @@
 pub mod challenge1;
+pub mod challenge2;
+// pub mod challenge3;
+// pub mod challenge4;
+// pub mod challenge5;
+// pub mod challenge6;
+// pub mod challenge7;
+// pub mod challenge8;
+// pub mod challenge9;
